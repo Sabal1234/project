74b21e1bd910e9351408be1df8b8176814eeae65
Sabal Adhikari <sadaladhikari@gmail.com>
2025-10-19 09:07:28 +0545

    update

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,10 @@ import dotenv from 'dotenv';
 import dbConnection from "./utils/index.js";
 
 dotenv.config();
-dbConnection()
+
 const app = express();
 const PORT = 5000;
-
+dbConnection()
 
 app.use(express.json());
 
@@ -15,4 +15,3 @@ app.use(express.json());
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-export { database };
